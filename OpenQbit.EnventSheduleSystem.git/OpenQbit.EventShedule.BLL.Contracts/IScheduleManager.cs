722fd778bbf6d60af53dcd30e80cc22a8d0ee6cb
Vinod Lakshan <Vinodlakshan16@gmail.com>
2017-07-01 06:29:22 +0530

    Creating Bll

---
+++ OpenQbit.EnventSheduleSystem.git/OpenQbit.EventShedule.BLL.Contracts/IScheduleManager.cs
@@ -0,0 +1,14 @@
+﻿using OpenQbit.EventShedule.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.EventShedule.BLL.Contracts
+{
+    public interface IScheduleManager
+    {
+        bool RecoredSchedule(Schedule schedule);
+    }
+}
---
+++ OpenQbit.EnventSheduleSystem.git/OpenQbit.EventShedule.BLL/ScheduleManager.cs
@@ -0,0 +1,31 @@
+﻿using OpenQbit.EventShedule.BLL.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.EventShedule.Common.Models;
+
+
+namespace OpenQbit.EventShedule.BLL
+{
+    public class ScheduleManager : IScheduleManager
+    {
+        private IRepository _repository;
+
+        private Ilogger _log;
+
+        [InjectionConstructor]
+        public ScheduleManager(IRepository repository, ILogger log)
+        {
+            this._repository = repository;
+            this._log = log;
+        }
+        public bool RecoredSchedule(Schedule schedule)
+        {
+            _log.LogError("");
+
+            return _repository.Create<Schedule>(schedule);
+        }
+    }
+}
