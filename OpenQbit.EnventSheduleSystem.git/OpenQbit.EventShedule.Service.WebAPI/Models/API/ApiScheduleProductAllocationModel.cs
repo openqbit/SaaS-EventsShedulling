72c203228b515aa3b4a9f93023b9e13aa9f0e971
Ulindu Anjula <ulinduanjula@gmail.com>
2017-07-01 14:48:41 -0700

    fixed

---
+++ OpenQbit.EnventSheduleSystem.git/OpenQbit.EventShedule.Service.WebAPI/Models/API/ApiScheduleProductAllocationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.EventShedule.Service.WebAPI.Models.API
+{
+    public class ApiScheduleProductAllocationModel
+    {
+        public int Id { get; set; }
+        public int ScheduleId { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public DateTime DepatureTime { get; set; }
+    }
+}
---
+++ OpenQbit.EnventSheduleSystem.git/OpenQbit.EventShedule.Service.WebAPI/Models/API/ApiSystemsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.EventShedule.Service.WebAPI.Models.API
+{
+    public class ApiSystemsModel
+    {
+        public int SystemsId { get; set; }
+        public string Description { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
