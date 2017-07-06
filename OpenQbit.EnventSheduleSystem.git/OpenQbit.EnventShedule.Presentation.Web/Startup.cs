14e9128f5a259ed9cfd6819553abe6a879f82601
ravinudwijepala <ravinduw119@gmail.com>
2017-07-06 07:46:43 +0530

    creating project ui

---
+++ OpenQbit.EnventSheduleSystem.git/OpenQbit.EnventShedule.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.EnventShedule.Presentation.Web.Startup))]
+namespace OpenQbit.EnventShedule.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
